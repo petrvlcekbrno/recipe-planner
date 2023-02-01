93e073ab6b5bac3886a2bf2f2808e26c8448a5a4
petrvlcekbrno <86242133+petrvlcekbrno@users.noreply.github.com>
2023-02-01 11:55:04 +0100

    teams replication

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = Franz => class Messenger extends Franz {
+module.exports = Franz => class Planner extends Franz {
   overrideUserAgent() {
-    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '');
+    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '').replace(/(Chrome\/)([^ ]*)/g, '$163.0.3239.84');
+  }
+
+  // https://docs.microsoft.com/en-us/microsoftteams/troubleshoot/teams-sign-in/sign-in-loop#resolution
+  knownCertificateHosts() {
+    return [
+      'microsoft.com',
+      'microsoftonline.com',
+      'teams.skype.com',
+      'teams.microsoft.com',
+      'sfbassets.com',
+      'skypeforbusiness.com',
+    ];
+  }
+
+  // we need to allow all cookies for ms teams
+  onHeadersReceived(details, callback) {
+    if (details.responseHeaders && details.responseHeaders['Set-Cookie'] && details.responseHeaders['Set-Cookie'].length && !details.responseHeaders['Set-Cookie'][0].includes('SameSite=none')) {
+      // eslint-disable-next-line no-param-reassign
+      details.responseHeaders['Set-Cookie'][0] = `${details.responseHeaders['Set-Cookie'][0]}; SameSite=none`;
+    }
+    callback({ cancel: false, responseHeaders: details.responseHeaders });
   }
 };
